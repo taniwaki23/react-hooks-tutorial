3f7be4e1fa79f3ea75b27ecfcf0b94ce348aaeec
shunsuke <shuntanaka01@outlook.jp>
2024-08-28 20:33:49 +0900

    [add]useEffect

--- src/lessons/Lesson2/Lesson2_1/Lesson2_1.tsx
+++ src/lessons/Lesson2/Lesson2_1/Lesson2_1.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from 'react';
+
 const Lesson2_1 = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    function handleMove(e: PointerEvent) {
+      setPosition({ x: e.clientX, y: e.clientY });
+    }
+    window.addEventListener('pointermove', handleMove);
+
+    return () => {
+      // アンマウント（DOMが削除されるとき）される直前に呼び出される。
+      window.removeEventListener('pointermove', handleMove);
+    };
+  }, []);
+
   return (
     <div
       style={{
-        position: "absolute",
-        backgroundColor: "blue",
-        borderRadius: "50%",
+        position: 'absolute',
+        backgroundColor: 'blue',
+        borderRadius: '50%',
         opacity: 0.6,
-        pointerEvents: "none",
+        pointerEvents: 'none',
+        transform: `translate(${position.x}px,${position.y}px)`,
         left: -20,
         top: -20,
         width: 50,

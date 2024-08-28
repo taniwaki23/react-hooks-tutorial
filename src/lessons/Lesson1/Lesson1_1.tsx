b78f22835fef7927bde0964a2bd85cdebcab1383
shunsuke <shuntanaka01@outlook.jp>
2024-08-28 18:26:54 +0900

    [add]useState

--- src/lessons/Lesson1/Lesson1_1.tsx
+++ src/lessons/Lesson1/Lesson1_1.tsx
@@ -1,9 +1,42 @@
+import { ChangeEvent, useState } from 'react';
+
 const Lesson1_1 = () => {
+  // state(age)の箇所だけ再レンダリングされるようになる
+  const [age, setAge] = useState<number>(0);
+  const [name, setName] = useState('demo');
+
+  const handleClick = () => {
+    setAge(age + 1);
+    // setAge((state) => state + 1);
+    // setAge((state) => state + 1);
+    // setAge((state) => state + 1);
+    /* 
+    setAge(age + 1);を複数実施しても、初期値(0)に対して実行されるので、値は3（3回実施)にはならず、1となる。
+    最後のsetAgeが終わったタイミングでレンダリングされる
+    setAge((state)=> state + 1)とすると更新用関数となるので、3とすることが可能。
+    */
+
+    console.log(name);
+  };
+
   return (
     <div>
-      <input type="text" />
-      <button className="border p-2 rounded-md bg-red-100">Add Age</button>
-      <p></p>
+      <input
+        type="text"
+        value={name}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+      />
+      {/* 
+      buttonのonClickについて、アロー関数を使う場合は、引数を指定する時に使う。
+      */}
+      <button
+        onClick={handleClick}
+        // onClick={() => handleClick()}
+        className="border p-2 rounded-md bg-red-100"
+      >
+        Add Age
+      </button>
+      <p>{age}</p>
     </div>
   );
 };
--- src/lessons/Lesson1/Lesson1_2.tsx
+++ src/lessons/Lesson1/Lesson1_2.tsx
@@ -1,23 +1,51 @@
+import { ChangeEvent, useState } from 'react';
+
 const Lesson1_2 = () => {
+  const [form, setForm] = useState({
+    firstName: 'abc',
+    lastName: 'def',
+    email: 'abcdef@gmail.com',
+  });
   return (
     <div>
       <div className="flex mb-5">
         <label>
           First Name:
-          <input type="text" className="border border-slate-500" />
+          <input
+            type="text"
+            className="border border-slate-500"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setForm({ ...form, firstName: e.target.value })
+            }
+          />
         </label>
         <label>
           Last Name:
-          <input type="text" className="border border-slate-500" />
+          <input
+            type="text"
+            className="border border-slate-500"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setForm({ ...form, lastName: e.target.value })
+            }
+          />
         </label>
         <label>
           Email:
-          <input type="text" className="border border-slate-500" />
+          <input
+            type="text"
+            className="border border-slate-500"
+            onChange={(e: ChangeEvent<HTMLInputElement>) =>
+              setForm({ ...form, email: e.target.value })
+            }
+          />
         </label>
       </div>
       <p>
+        {form.firstName}
         <br />
+        {form.lastName}
         <br />
+        {form.email}
       </p>
     </div>
   );

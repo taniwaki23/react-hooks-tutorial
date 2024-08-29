efd5650f46a2f5b3abc058bb5ad4a04099c79bf2
shunsuke <shuntanaka01@outlook.jp>
2024-08-29 19:36:45 +0900

    [fix]パフォーマンス修正

--- src/lessons/Lesson5/Lesson5_2/Lesson5_2.tsx
+++ src/lessons/Lesson5/Lesson5_2/Lesson5_2.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
-import { useToggle } from "./hooks/useToggle";
+import { useState } from 'react';
+import { useToggle } from './hooks/useToggle';
 
 const Lesson5_2 = () => {
   const [count, setCount] = useState(0);
-  const [on, toggle] = useToggle(false);
 
-  console.log("Parent rendered");
+  console.log('Parent rendered');
 
   return (
     <div>
@@ -16,7 +15,7 @@ const Lesson5_2 = () => {
       >
         Parent Count
       </button>
-      <Child toggle={toggle} on={on} />
+      <Child />
     </div>
   );
 };
@@ -24,13 +23,14 @@ const Lesson5_2 = () => {
 export default Lesson5_2;
 
 // eslint-disable-next-line react-refresh/only-export-components
-const Child = ({ toggle, on }: { toggle: () => void; on: boolean }) => {
-  console.log("Child rendered");
+const Child = () => {
+  const [on, toggle] = useToggle(false);
+  console.log('Child rendered');
   let i = 0;
   while (i < 10000000) i++;
   return (
     <div>
-      <p>Child {on ? "ON" : "OFF"}</p>
+      <p>Child {on ? 'ON' : 'OFF'}</p>
       <button onClick={toggle} className="border-2 px-2 py-2 rounded-md">
         Toggle
       </button>

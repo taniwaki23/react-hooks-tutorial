cbf7dd1bc3090446ef50213db66b84d91ad593b1
shunsuke <shuntanaka01@outlook.jp>
2024-08-29 20:07:54 +0900

    [add]useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Lesson5_2 from './lessons/Lesson5/Lesson5_2/Lesson5_2';
+import Lesson5_3 from './lessons/Lesson5/Lesson5_3/Lesson5_3';
 
 function App() {
   return (
     <div className="flex items-center max-w-4xl mx-auto py-8 text-2xl">
-      <Lesson5_2 />
+      <Lesson5_3 />
     </div>
   );
 }

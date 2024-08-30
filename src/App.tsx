84faa04dec97fe5425c58945185a7991a8413e17
shunsuke <shuntanaka01@outlook.jp>
2024-08-30 09:23:09 +0900

    [add]useTransition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import './App.css';
-import Lesson7_2 from './lessons/Lesson7/Lesson7_2/Lesson7_2';
+import Lesson7_1 from './lessons/Lesson7/Lesson7_1/Lesson7_1';
 
 function App() {
   return (
     <div className="flex items-center max-w-4xl mx-auto py-8 text-2xl">
-      <Lesson7_2 />
+      <Lesson7_1 />
     </div>
   );
 }
